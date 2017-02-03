57ba54921855527d1d2c2191ae71aff5c7afe269
Jorge Ramón <jorg3.r4mon@gmail.com>
2017-02-03 15:26:03 -0600

    Correccion al guardado

--- Backend/services/ScoreService.js
+++ Backend/services/ScoreService.js
@@ -7,7 +7,7 @@ class ScoreService {
 
 	getScores(callback) { ScoreModel.find({}).sort({ score: 1 }).exec(callback) }
 
-	saveScore(score, callback) { ScoreModel.save(new ScoreSchema(score), callback) }
+	saveScore(score, callback) { ScoreModel.save(score, callback) }
 }
 
 module.exports = ScoreService;
