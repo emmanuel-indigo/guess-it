50d1da4bf679f2cc40ee22ffa7b4d63186be33b7
Jorge Ramón <jorg3.r4mon@gmail.com>
2017-02-03 15:39:03 -0600

    Se almacena el score

--- Backend/schemas/ScoreSchema.js
+++ Backend/schemas/ScoreSchema.js
@@ -3,6 +3,6 @@ var mongoose = require("mongoose");
 var ScoreSchema = mongoose.Schema({
 	score: Number,
 	name: String
-});
+}, { collection: 'scores' });
 
 module.exports = ScoreSchema;
--- Backend/services/ScoreService.js
+++ Backend/services/ScoreService.js
@@ -1,13 +1,16 @@
 var mongoose = require("mongoose");
 var ScoreSchema = require("../schemas/ScoreSchema");
 
-var ScoreModel = mongoose.model('Score', ScoreSchema);	
+var ScoreModel = mongoose.model('score', ScoreSchema);	
 
 class ScoreService {
 
 	getScores(callback) { ScoreModel.find({}).sort({ score: 1 }).exec(callback) }
 
-	saveScore(score, callback) { ScoreModel.save(score, callback) }
+	saveScore(score, callback) { 
+		var score = new ScoreModel(score);
+		score.save(callback);
+	}
 }
 
 module.exports = ScoreService;
